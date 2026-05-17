5d109815599dae1cb16cc1e102b3b7093e7168b7
Null Cherry <sk4ndi1488@gmail.com>
2026-05-17 21:33:53 +0300

    Update Library.lua

--- Files/Library.lua
+++ Files/Library.lua
@@ -4710,7 +4710,9 @@ end
 
 local cache = { }
 local function getIcon(value, list, object)
+    value = value or ""
     local str = tostring(value) .. tostring(list)
+    
     if cache[str] then
         return cache[str]
     end
@@ -6283,7 +6285,7 @@ local tabFuncs = {
         end)
 
         cons[#cons + 1] = tabButton.ButtonItself.Icon.Changed:Connect(function()
-            tabButton.ButtonItself.Icon.Visible = not not (options.Icon and options.Icon ~= "")
+            tabButton.ButtonItself.Icon.Visible = getIcon(options.Icon, icons, object.Proxy)
         end)
 
         tab.NormalZone.ChildAdded:Connect(function(child)
@@ -6351,9 +6353,6 @@ local tabFuncs = {
         if dont then return self end
 
         local options = self.Options
-        if tonumber(options.Icon) then
-            options.Icon = "rbxassetid://" .. tostring(options.Icon)
-        end
 
         self.TabButton.Parent = self.Parent.Window.RealWindow.Contents.Display.PageButtons.List
         self.Holder.Parent = self.Parent.Window.RealWindow.Contents.Display.Pages
@@ -7055,7 +7054,7 @@ local windowFuncs; windowFuncs = {
         end)
 
         cons[#cons + 1] = titleZone.Icon.Changed:Connect(function()
-            titleZone.Icon.Visible = not not (options.Icon and (options.Icon:find("rbxassetid://", 0, true) or options.Icon:find("rbxasset://", 0, true)))
+            titleZone.Icon.Visible = getIcon(options.Icon, icons, object.Proxy) ~= ""
         end)
 
         local footer = window.RealWindow.Contents.Footer.Label
@@ -7921,6 +7920,8 @@ local insert = table.insert
 local remove = table.remove
 local rawset = rawset
 local unpack = table.unpack
+local defer = task.defer
+local delay = task.delay
 
 local quick = Instance.new("BindableEvent") -- only for instant :Wait response
 local quickEvent = quick.Event
@@ -7935,13 +7936,12 @@ local connectionBase = {
         end
     end,
     Fire = function(self, ...)
-        if not self.Enabled or not self.Connected then return end
+        if not self.Enabled or not self.Connected or not self.Parent.Enabled then return end
         spawn(self.Callback, ...)
     end
 }
 
-connectionBase = { __index = connectionBase }
-
+connectionBase = { __index = connectionBase, __tostring = function() return "Connection" end, __newindex = function(self, key, value) if key ~= "Enabled" then error() end rawset(self, key, value) end }
 local eventBase = {
     Connect = function(self, func)
         local connection = smt({ Callback = func, Connected = true, Enabled = true, Parent = self }, connectionBase)
@@ -7999,16 +7999,76 @@ local eventBase = {
     end
 }
 
-eventBase = { __index = eventBase }
-
-local new = function()
-    return smt({ _Connections = { } }, eventBase)
-end
-
+eventBase = { __index = eventBase, __tostring = function() return "Event" end, __newindex = function(self, key, value) if key ~= "Enabled" then error() end rawset(self, key, value) end }
 local lib = setmetatable({
-    new = new
-}, { __call = function(_, ...) return new(...) end })
+    new = function()
+        return smt({ _Connections = { }, Enabled = true }, eventBase)
+    end,
+    RaceEvents = function(self, ...)
+        local events = { ... }
+        if typeof(events[1]) == "table" then
+            events = events[1]
+        end
+        
+        if #events == 0 then return end
+        if #events == 1 then return events[1]:Wait() end
+        
+        local result, winner
+        local connections = { }
+        
+        for i, v in events do
+            connections[#connections + 1] = v:Once(function(...)
+                for i, v in connections do
+                    v:Disconnect()
+                end
+                
+                winner = v
+                result = pack(...)
+                quick:Fire()
+            end)
+        end
+        
+        repeat quickEvent:Wait() until result
+        
+        insert(result, 1, winner)
+        return unpack(result, 1, result.n + 1)
+    end
+}, freeze({ __call = function(self, ...) return self.new(...) end }))
 global[n] = lib
+
+local clock, fakeClock, deferClock = lib.new(), lib.new(), lib.new()
+local race = lib.RaceEvents
+local rs = game:GetService("RunService")
+local r1, r2, r3 = rs.RenderStepped, rs.Heartbeat, rs.Stepped
+local last = tick()
+local fire = clock.Fire
+local maxDefer = 10
+
+clock:Connect(function(isDefer, dontFire)
+    local current = tick()
+    fakeClock:Fire(current - last, isDefer)
+    last = current
+    
+    if dontFire then return end
+    
+    race(clock, r1, r2, r3)
+    fire(clock, false, false)
+    delay(0, fire, clock, false, true)
+    
+    for i = 1, maxDefer do
+        defer(fire, deferClock)
+        deferClock:Wait()
+        
+        if i <= 3 or i == 10 or i == maxDefer then
+            fire(clock, true, true)
+            delay(0, fire, clock, false, true)
+        end
+    end
+end)
+
+rawset(lib, "Clock", fakeClock)
+freeze(lib)
+fire(clock, false)
 
 return lib
     end;
